2f2416ce7ded98c101e096448b7a9d1cf863bc6c
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-09-24 18:01:53 +0700

    first try Responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,22 @@
+import { useMediaQuery } from 'react-responsive';
+
 import Content from 'layout/Content'
 
 import logoApparel from 'assets/images/logo.svg';
 import heroDesktop from 'assets/images/hero-desktop.jpg';
+import heroMobile from 'assets/images/hero-mobile.jpg';
 
 const Home = () => {
+
+  const isTablet = useMediaQuery({ query: '(min-width: 960px)' })
+  const isMobile = useMediaQuery({ query: '(max-width: 480px)' })
+
     return (
       <main>
         <img src={logoApparel} alt="" className="logo" />
         <Content />
-        <img src={heroDesktop} alt="" className="hero" />
+        {isTablet && <img src={heroDesktop} alt="" className="hero" />}
+        {isMobile && <img src={heroMobile} alt="" className="hero-mobile" />}
       </main>
     );
 }
